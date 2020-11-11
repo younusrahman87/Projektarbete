05cdd34d0e315d7d3570fab7c32a9cf81800aee8
MaahdiZ <70632771+MaahdiZ@users.noreply.github.com>
2020-11-11 12:19:07 +0100

    Add files via upload

---
+++ Alla Mekaniker.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI.Pages
+{
+    /// <summary>
+    /// Interaction logic for Alla_Mekaniker.xaml
+    /// </summary>
+    public partial class Alla_Mekaniker : Page
+    {
+        public Alla_Mekaniker()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Search_Mechanic.Visibility = Visibility.Visible;
+            // tb_SC_mechanic_id.Text = IUserDataAccess.Read()
+            // tb_SC_mechanic_name = IUserDataAccess.Read()
+            // tb_SC_mechanic_birthday = IUserDataAccess.Read()
+            // tb_SC_mechanic_skills = IUserDataAccess.Read()
+            // tb_SC_mechanic_job1
+            // tb_SC_mechanic_job2
+        }
+    }
+}
