cd1b191911895d0f4333ff95e617b3e4d5aa4430
rocknrollaswe <70640004+rocknrollaswe@users.noreply.github.com>
2020-11-16 08:27:47 +0100

    Add files via upload

---
+++ KodaMHakan/KodaMHåkan/Car.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KodaMHåkan
+{
+    class Car
+    {
+
+        private string licenseNumber;
+
+        private string color;
+
+        public string LicenseNumber 
+        {
+            get {return "*** " + licenseNumber + " ***";}
+            set 
+            {
+                string letters = value.Substring(0, 3);
+                
+                bool isNumbers = int.TryParse(value.Substring(3, 3), out int numbers);
+
+                if (value.Length != 6)
+                {
+                    throw new Exception("Registreringsnumret du angett är för långt/kort!");
+
+                }
+                //(letters.Any(c => char.IsDigit(c)))
+                
+                if (letters.Any(char.IsDigit))
+                {
+
+                    throw new Exception("Registreringsnumret du angett får inte ange " +
+                        "siffror som någon av de tre första karaktärerna");
+
+                }
+                if (!isNumbers)
+                {
+                    throw new Exception("Registreringsnumret du angett får inte ange " +
+                        "bokstäver som någon av de tre sista karaktärerna");
+                }
+
+                if (letters.Contains("KUK"))// Mycket fult ord
+                {
+                    throw new Exception("Olämpligt regNummer");
+
+                }
+
+                licenseNumber = value;
+            }
+
+        }
+
+        public string Color 
+        {
+            get
+            {
+                if (color == "red")
+                {
+                    return "röd".ToUpper(); 
+                }
+                if (color == "green")
+                {
+                    return "grön".ToUpper();
+                }
+                if (color == "blue")
+                {
+                    return  "blå".ToUpper();
+                }
+                if (color == "yellow".ToUpper())
+                {
+                    return  "gul".ToUpper();
+                }
+                return color; 
+               
+            } 
+            
+            set 
+            {
+                value = value.ToLower(); 
+
+                if (value == "red" || value == "green" || value == "blue" || value == "yellow")
+                {
+                    color = value;
+                }
+                else
+                    throw new Exception("Färgen du angett ingår inte i paletten");
+            }
+
+        }
+
+        //public void SetLicenseNumber(string lNumber)
+        //{
+        //    string letters = lNumber.Substring(0, 3);
+        //    int numbers = 0;
+        //    bool isNumbers = int.TryParse(lNumber.Substring(3, 3), out numbers);
+
+        //    if (lNumber.Length != 6)
+        //    {
+        //        throw new Exception("Registreringsnumret du angett är för långt/kort!");
+
+        //    }
+        //    if (letters.All(char.IsDigit))
+        //    {
+
+        //        throw new Exception("Registreringsnumret du angett får inte ange " +
+        //            "siffror som någon av de tre första karaktärerna");
+
+        //    }
+        //    if (!isNumbers)
+        //    {
+        //        throw new Exception("Registreringsnumret du angett får inte ange " +
+        //            "bokstäver som någon av de tre sista karaktärerna");
+        //    }
+
+        //    if (letters.Contains("KUK"))
+        //    {
+        //        throw new Exception("Olämpligt regNummer");
+
+        //    }
+
+        //licenseNumber = lNumber;
+
+        //}
+
+        //public void SetColor(string c) 
+        //{
+        //    if (c.ToLower() == "red" || c.ToLower() == "green" || c.ToLower() == "blue" || c.ToLower() == "yellow")
+        //    {
+        //        color = c;
+        //    }
+        //    else
+        //        throw new Exception("Färgen du angett ingår inte i paletten"); 
+        //}
+
+        //public string GetColor() 
+        //{
+        //    string colorSvenska = null; 
+
+        //    if (color == "red") 
+        //    {
+        //        colorSvenska = "röd";
+        //    }
+        //    if (color == "green")
+        //    {
+        //        colorSvenska = "grön";
+        //    } 
+        //    if (color == "blue")
+        //    {
+        //        colorSvenska = "blå";
+        //    }
+        //    if (color == "yellow")
+        //    {
+        //        colorSvenska = "gul";
+        //    }
+
+        //    return colorSvenska; 
+        //}
+
+
+
+    }
+}
---
+++ KodaMHakan/KodaMHåkan/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.ComponentModel.Design;
+
+namespace KodaMHåkan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            bool skapaBilar = true;
+            string answer;
+            int index = 0;
+
+            Car[] cars = new Car[100]; 
+
+            while (skapaBilar) 
+            {
+               
+                cars[index] = AddCar(); 
+                do
+                {
+                    Console.WriteLine("Är du färdig med att skapa upp bilar? J/N?");
+                    answer = Console.ReadLine();
+                    if (answer.ToLower() == "j")
+                    {
+                        skapaBilar = false;
+                    }
+                    else if (answer.ToLower() == "n")
+                    {
+                        break;
+                    }
+                    else
+                        Console.WriteLine();
+                }
+                while (answer.ToLower() != "j");
+                
+                index++;
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                if (cars[i].Color == "GUL")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                else if (cars[i].Color == "GRÖN")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (cars[i].Color == "BLÅ")
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else if (cars[i].Color == "RÖD")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                Console.WriteLine($"RegistreringsNummer: {cars[i].LicenseNumber}");
+                Console.WriteLine($"Bilens Färg: {cars[i].Color}\n------------------------------------------------");
+            }
+        }
+   
+        public static Car AddCar() 
+        {
+            Car car = new Car();
+            Console.WriteLine("Var vänlig lägg till en bil! Skriv regnummer!");
+            string regNummer = Console.ReadLine();
+            car.LicenseNumber = regNummer;
+            Console.WriteLine("Och var vänlig ange färg!");
+            string bilFarg = Console.ReadLine(); 
+            car.Color = bilFarg;
+            
+            return car;
+        }
+    
+    }
+}
